99db3eb3574542ffe7033588a69070bc3802b1f7
Lucas Bovolini <lucas.bovolini@zup.com.br>
2021-09-28 11:10:28 -0300

    Setup do projeto

---
+++ src/main/kotlin/com/github/zlbovolini/Application.kt
@@ -0,0 +1,11 @@
+package com.github.zlbovolini
+
+import io.micronaut.runtime.Micronaut.*
+
+fun main(args: Array<String>) {
+	build()
+	    .args(*args)
+		.packages("com.github.zlbovolini")
+		.start()
+}
+
